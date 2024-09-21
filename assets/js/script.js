e14f7c55038e68da397b668151dc87aef7892a0b
Stephen <stephen_salamante@dlsu.edu.ph>
2024-09-21 15:39:56 +0800

    new face

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,39 @@
-// ScrollReveal({ reset: true }).reveal('.header-container');
-// ScrollReveal().reveal('.header-container');
-// ScrollReveal().reveal('.body-container');
+dragElement($('.move-1')[0]);
+dragElement($('.move-2')[0]);
+dragElement($('.move-3')[0]);
+dragElement($('.move-4')[0]);
 
-// var prevScrollpos = window.pageYOffset;
-// window.onscroll = function () {
-//   var currentScrollPos = window.pageYOffset;
-//   if (prevScrollpos > currentScrollPos) {
-//     document.getElementsByClassName("navbar").style.top = "0";
-//   } else {
-//     document.getElementsByClassName("navbar").style.top = "-50px";
-//   }
-//   prevScrollpos = currentScrollPos;
-// }
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    elmnt.onmousedown = dragMouseDown;
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
